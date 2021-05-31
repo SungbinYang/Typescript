25765dc6061a4b88c934311fb2978a42a561a72c
Robert.Yang <ysb05222@naver.com>
2021-05-31 22:41:50 +0900

    array
    array

---
+++ array.js
@@ -0,0 +1,3 @@
+"use strict";
+var list1 = [1, 2, 3, "4"]; // 이 방법 선호
+var list2 = [1, 2, 3];
---
+++ array.ts
@@ -0,0 +1,3 @@
+let list1: (number | string)[] = [1, 2, 3, "4"]; // 이 방법 선호
+
+let list2: Array<number> = [1, 2, 3];
