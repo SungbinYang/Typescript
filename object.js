fb352f31ebc46688c259ec5f255141867372be61
Robert.Yang <ysb05222@naver.com>
2021-05-31 22:31:35 +0900

    object
    object

---
+++ object.js
@@ -0,0 +1,3 @@
+"use strict";
+var person1 = { name: "Mark", age: 39 }; // type : { name: "Mark", age: 39 } type
+var person2 = Object.create({ name: 'Mark', age: 39 });
---
+++ object.ts
@@ -0,0 +1,3 @@
+const person1 = { name: "Mark", age: 39 }; // type : { name: "Mark", age: 39 } type
+
+const person2 = Object.create({ name: 'Mark', age: 39 });
