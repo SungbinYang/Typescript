0d67e55ae8fa780be8fce7c07592b6e398c13c7d
Robert.Yang <ysb05222@naver.com>
2021-05-31 22:48:54 +0900

    tuple
    tuple

---
+++ tuple.js
@@ -0,0 +1,6 @@
+"use strict";
+var x; // 튜플 자료형
+x = ["hello", 39];
+// x = [10, "Mark"];
+var person = ["Rovert", 39];
+var first = person[0], second = person[1]; // 분해할당
---
+++ tuple.ts
@@ -0,0 +1,9 @@
+let x: [string, number]; // 튜플 자료형
+
+x = ["hello", 39];
+
+// x = [10, "Mark"];
+
+const person: [string, number] = ["Rovert", 39];
+
+const [first, second] = person; // 분해할당
