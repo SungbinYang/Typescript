e65c766df9bcf14e19e0611cd43c7d381efd1d43
Robert.Yang <ysb05222@naver.com>
2021-06-04 23:53:53 +0900

    Delete imterface directory

--- imterface/interface1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function hello1(person) {
-    console.log("\uC548\uB155\uD558\uC138\uC694! " + person.name + " \uC785\uB2C8\uB2E4.");
-}
-var p1 = {
-    name: 'Rovert',
-    age: 26
-};
-hello1(p1);
