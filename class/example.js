8230986fee5c8c978424d5f4f5f34131197ad717
Robert.Yang <ysb05222@naver.com>
2021-06-06 15:23:32 +0900

    constructor & initialize
    constructor & initialize

--- class/example.js
+++ class/example.js
@@ -1,8 +1,11 @@
 "use strict";
 class Person {
-    constructor(name) {
-        this.name = name;
+    constructor(age) {
+        this.name = "Rovert";
+        this.age = age;
     }
 }
-const p1 = new Person("Rovert");
+const p1 = new Person(26);
 console.log(p1);
+// p1.age = 26;
+console.log(p1.age); // 직접 할당을 했으므로 값은 잘 나오지만 런타임상 클래스는 모른다.
--- class/example.ts
+++ class/example.ts
@@ -1,12 +1,24 @@
 class Person {
+  name: string = "Rovert";
+  age!: number; // 개발자가 어디선가 값을 할당할것이라고 의도적으로 명시하기 위해 !붙여놓음 생성자에 할당할 경우 !생략 가능
 
-  name: string;
+  constructor(age?: number) { // constructor에서는 async를 할수 없다.
+    if (age === undefined) {
+      this.age = 20;
+    } else {
+      this.age = age;
+    }
+  }
 
-  constructor(name: string) {
-    this.name = name;
+  async init() {
+
   }
 }
 
-const p1 = new Person("Rovert");
+const p1: Person = new Person(26);
+// await p1.init();
+const p2: Person = new Person();
 
 console.log(p1);
+// p1.age = 26;
+console.log(p1.age); // 직접 할당을 했으므로 값은 잘 나오지만 런타임상 클래스는 모른다.
