6e27ac74f052ba1221a7f63ad338f46de0ab9ab2
Robert.Yang <ysb05222@naver.com>
2021-06-06 14:58:07 +0900

    What are Classes
    What are Classes

---
+++ class/example.js
@@ -0,0 +1,8 @@
+"use strict";
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const p1 = new Person("Rovert");
+console.log(p1);
---
+++ class/example.ts
@@ -0,0 +1,12 @@
+class Person {
+
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const p1 = new Person("Rovert");
+
+console.log(p1);
