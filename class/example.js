deaf32f4243ccd11f312850a8e0477ee53407380
Robert.Yang <ysb05222@naver.com>
2021-06-06 15:44:46 +0900

    접근제어자
    접근제어자

--- class/example.js
+++ class/example.js
@@ -1,11 +1,31 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 class Person {
     constructor(age) {
         this.name = "Rovert";
-        this.age = age;
+        if (age === undefined) {
+            this.age = 20;
+        }
+        else {
+            this.age = age;
+        }
+    }
+    init() {
+        return __awaiter(this, void 0, void 0, function* () {
+        });
     }
 }
 const p1 = new Person(26);
+// await p1.init();
+const p2 = new Person();
 console.log(p1);
 // p1.age = 26;
-console.log(p1.age); // 직접 할당을 했으므로 값은 잘 나오지만 런타임상 클래스는 모른다.
+// console.log(p1.age); // 직접 할당을 했으므로 값은 잘 나오지만 런타임상 클래스는 모른다.
--- class/example.ts
+++ class/example.ts
@@ -1,8 +1,8 @@
 class Person {
-  name: string = "Rovert";
-  age!: number; // 개발자가 어디선가 값을 할당할것이라고 의도적으로 명시하기 위해 !붙여놓음 생성자에 할당할 경우 !생략 가능
+  public name: string = "Rovert";
+  private age!: number; // 개발자가 어디선가 값을 할당할것이라고 의도적으로 명시하기 위해 !붙여놓음 생성자에 할당할 경우 !생략 가능
 
-  constructor(age?: number) { // constructor에서는 async를 할수 없다.
+  public constructor(age?: number) { // constructor에서는 async를 할수 없다.
     if (age === undefined) {
       this.age = 20;
     } else {
@@ -10,7 +10,7 @@ class Person {
     }
   }
 
-  async init() {
+  public async init() {
 
   }
 }
@@ -21,4 +21,4 @@ const p2: Person = new Person();
 
 console.log(p1);
 // p1.age = 26;
-console.log(p1.age); // 직접 할당을 했으므로 값은 잘 나오지만 런타임상 클래스는 모른다.
+// console.log(p1.age); // 직접 할당을 했으므로 값은 잘 나오지만 런타임상 클래스는 모른다.
